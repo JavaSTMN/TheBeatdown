ec4c855fcab3aa92575c960c3ffabde1c14511d8
Thomas Cianfarani <thomas.ci83@gmail.com>
2019-11-19 14:15:31 +0100

    Update ctrler

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,12 +5,21 @@ import javafx.fxml.Initializable;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-import java.io.File;
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+/**
+ * Controller lié au fichier FXML principal, permettant la maj des éléments d'UI.
+ * Pour accéder à un composant de l'UI, il faut aller dans le FXML correspondant et donner un id au composant voulu.
+ * Ensuite, dans le code ici, on déclare une var privée du type du composant, avec @FXML au dessus.
+ * Voir les examples ci-dessous.
+ */
 public class Controller implements Initializable {
+    public static Controller instance;
+    public static Controller getInstance() {
+        return instance;
+    }
+
     @FXML
     private ImageView player1Portrait;
 
@@ -24,7 +33,20 @@ public class Controller implements Initializable {
      */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        renderEverything();
+    }
+
+    private void renderEverything() {
+        renderHeroes();
+        // TODO: Render mana reserves
+        // TODO: Render hands
+        // TODO: Render decks
+        // TODO: Render boards
+    }
+
+    public void renderHeroes() {
         initHeroesPortraits();
+        // TODO: Render rest of heroes stuff
     }
 
     private void initHeroesPortraits() {
