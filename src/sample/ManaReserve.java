c7151ebee43b5551fc92490cb96c3b36b34f1826
Thomas Cianfarani <thomas.ci83@gmail.com>
2019-11-26 11:33:29 +0100

    Fix mana increase issue

--- src/sample/ManaReserve.java
+++ src/sample/ManaReserve.java
@@ -37,9 +37,9 @@ public class ManaReserve {
 
     public void addManaMax(int amount) {
         int manaMax = getMaxMana();
-        int slotsMax=getMaxManaSlots();
-        if((manaMax+amount)<slotsMax){
-            setMaxMana(manaMax+amount);
+        int slotsMax = getMaxManaSlots();
+        if((manaMax+amount) <= slotsMax){
+            setMaxMana(manaMax + amount);
         }
     }
 
